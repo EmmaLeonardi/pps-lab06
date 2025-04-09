21b27f11f348d736cde284aad847faa0039041b9
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2025-04-09 19:55:24 +0200

    Implemented ConferenceReviewing, started testing it

---
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -0,0 +1,104 @@
+package ex2
+
+import scala.language.postfixOps
+
+trait ConferenceReviewing:
+  enum Question:
+    case RELEVANCE, SIGNIFICANCE, CONFIDENCE, FINAL
+
+  /**
+   * @param article the number of the article
+   * @param scores  the scores of the questions
+   *                loads a review for the specified article, with complete scores as a map
+   */
+  def loadReview(article: Int, scores: Map[Question, Int]): Unit
+
+  /**
+   * @param article      the number of the article
+   * @param relevance    points, between 0 and 10
+   * @param significance points, between 0 and 10
+   * @param confidence   points, between 0 and 10
+   * @param fin          points, between 0 and 10
+   *                     loads a review for the specified article, with the 4 explicit scores
+   */
+  def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit
+
+  /**
+   * @param article  the number of the article
+   * @param question number
+   * @return the scores given to the specified article and specified question, as an (ascending-ordered) list
+   */
+  def orderedScores(article: Int, question: Question): List[Int]
+
+  /**
+   * @param article the number of the article
+   * @return the average score to question FINAL taken by the specified article
+   */
+  def averageFinalScore(article: Int): Double
+
+  /**
+   * An article is considered accepted if its averageFinalScore (not weighted) is > 5,
+   * and at least one RELEVANCE score that is >= 8.
+   *
+   * @return the set of accepted articles
+   */
+  def acceptedArticles(): Set[Int]
+
+  /**
+   * @return accepted articles as a list of pairs article+averageFinalScore, ordered from worst to best based on averageFinalScore
+   */
+  def sortAcceptedArticles(): List[(Int, Double)]
+
+  /**
+   * @return a map from articles to their average "weighted final score", namely,
+   *         the average value of CONFIDENCE*FINAL/10
+   *         Note: this method is optional in this exam
+   */
+  def averageWeightedFinalScoreMap(): Map[Int, Double]
+
+object ConferenceReviewing:
+  def apply(): ConferenceReviewing = ConferenceReviewingImpl()
+
+case class ConferenceReviewingImpl() extends ConferenceReviewing:
+
+  private var conferenceReviews = List[(Int, Map[Question, Int])]()
+
+  def loadReview(article: Int, scores: Map[Question, Int]): Unit =
+    require(scores.size < Question.values.length)
+    conferenceReviews = (article, scores) :: conferenceReviews
+
+  def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit =
+    require(relevance > 0 && relevance < 10)
+    require(significance > 0 && significance < 10)
+    require(confidence > 0 && confidence < 10)
+    require(fin > 0 && fin < 10)
+    conferenceReviews = (article, Map[Question, Int](Question.RELEVANCE -> relevance, Question.SIGNIFICANCE -> significance, Question.CONFIDENCE -> confidence, Question.FINAL -> fin)) :: conferenceReviews
+
+  def orderedScores(article: Int, question: Question): List[Int] = conferenceReviews.collect { case a if a._1 == article => a._2(question) }.sorted
+
+  def averageFinalScore(article: Int): Double =
+    val fin = conferenceReviews.collect { case a if a._1 == article => a._2(Question.FINAL) }
+    fin.sum / fin.length
+
+  private def accepted(article: Int): Boolean =
+    this.averageFinalScore(article) > 5 && conferenceReviews.collect { case a if a._1 == article => a._2 }.exists(a => a.contains(Question.RELEVANCE) && a(Question.RELEVANCE) >= 8)
+
+  def acceptedArticles(): Set[Int] = conferenceReviews.map(a => a._1).distinct.filter(a => accepted(a)).toSet
+
+  def sortAcceptedArticles(): List[(Int, Double)] = this.acceptedArticles().map(a => (a, this.averageFinalScore(a))).toList.sortBy(_._2)
+
+  private def averageWeightedFinalScore(article: Int): Double =
+    val total = conferenceReviews.collect { case a if a._1 == article => a._2(Question.FINAL) * a._2(Question.CONFIDENCE) / 10 }
+    total.sum / total.length
+
+  def averageWeightedFinalScoreMap(): Map[Int, Double] = conferenceReviews.map(a => a._1).distinct.map(a => (a, averageWeightedFinalScore(a))).toMap
+
+  override def toString: String = conferenceReviews.toString()
+
+@main def Main: Unit =
+  //import ConferenceReviewing.
+  val confrev = ConferenceReviewing()
+  println(confrev) //0
+  confrev.loadReview(article = 1, relevance = 8, significance = 8, confidence = 6, fin = 8)
+  println(confrev) //1
+  //TODO: finish testing
--- src/main/scala/ex2/README.md
+++ src/main/scala/ex2/README.md
@@ -2,3 +2,17 @@
 - take a look to this [oop exam](https://bitbucket.org/mviroli/oop2018-esami/src/master/a03b/e1/Test.java)
 - implement the solution using the scala collection library
 - follow the line guides described in class (immutable var over mutable val, ...)
+
+
+Si consulti la documentazione dell'interfaccia ConferenceReviewing, che modella i risultati del processo di revisione
+degli articoli di una conferenza. Ogni articolo viene da revisionato da uno o più revisori anonimi, ognuno dei quali fornisce
+una valutazione (score) da 0 a 10 per 4 diverse "domande", modellate da ConferenceReviewing.Question. Un articolo viene
+accettato se il valore medio della valutazione alla domanda "FINAL" è >5 e se ha almeno una valutazione "RELEVANCE" >= 8.
+
+Implementare ConferenceReviewing attraverso una classe ConferenceReviewingImpl con costruttore senza argomenti,
+in modo che passi tutti i test di cui sotto, realizzati per essere autoesplicativi.
+
+Sono considerati opzionali ai fini della possibilità di correggere l'esercizio, ma concorrono comunque al raggiungimento
+della totalità del punteggio:
+- implementazione dei test opzionali (relativi alla realizzazione del metodo averageWeightedFinalScoreMap)
+- la qualità della soluzione, in particolare con minimizzazione di ripetizioni e codice non inutilmente complesso
